1b5d736cdc933475d36da619e6207c18d4fb7a36
0x6c3067316362306d62 <serwickijakub+github@gmail.com>
2024-05-17 21:40:06 +0200

    scripts

---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+import yaml
+import logging
+import logging.config
+
+
+def setup_logging(default_path='config/logging.yaml', default_level=logging.DEBUG):
+    path = default_path
+
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            try:
+                config = yaml.safe_load(f.read())
+                logging.config.dictConfig(config)
+            except Exception as e:
+                print(e)
+                print('Error in Logging Configuration. Using default configs')
+                logging.basicConfig(level=default_level)
+    else:
+        logging.basicConfig(level=default_level)
+        print('Failed to load configuration file. Using default configs')
+
+
+def main():
+    setup_logging()
+
+    logger = logging.getLogger(__name__)
+
+    logger.debug('debug message')
+    logger.info('info message')
+    logger.warning('warn message')
+    logger.error('error message')
+    logger.critical('critical message')
+
+
+if __name__ == '__main__':
+    main()
+
+
+
